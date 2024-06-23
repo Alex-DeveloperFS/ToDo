51ed7bda561e08277cbf6eee908e3fe8782017eb
Alex-DeveloperFS <iotov.developer@gmail.com>
2024-06-23 17:56:29 +0300

    first commit

---
+++ ToDo/src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import Todo from "./Todo.tsx"
+import {TodosPropsInterface} from "../types/TodosProps.interface.ts"
+
+const Todos = ({todos, deleteTodo}: TodosPropsInterface) => {
+  console.log(todos)
+
+  return (
+    <div>
+      {todos.length === 0 && <p className="todos__empty">Todos list is empty</p>}
+      {todos.map((todo) => <Todo todo={todo} deleteTodo={deleteTodo} key={todo.id}/>)}
+    </div>
+  )
+}
+export default Todos
